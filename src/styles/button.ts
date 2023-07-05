0f9649206af6e60a1debf15d02d53f126d519d49
leafdesk <lsg001008@gmail.com>
2023-07-05 15:13:13 +0900

    :lipstick: 스타일 수정 - custom button style 적용. (height 고정)

--- src/styles/button.ts
+++ src/styles/button.ts
@@ -1,7 +1,16 @@
 import { primary, alternative, dark, light, green, red, yellow, purple } from '@/styles/colors'
 
-// mr-2 mb-2 ?
-export const button = 'focus:outline-none focus:ring-4 font-medium px-2 py-2 w-10 h-10 text-sm'
+export const buttonOutline = 'focus:outline-none focus:ring-4'
+
+/**
+ * [ORIGINAL] py-2.5 는 flex 안에서 무시되고, content-fit height 가 적용됨.
+ */
+export const _button = `${buttonOutline} font-medium text-sm px-5 py-2.5`
+
+/**
+ * [CUSTOM] input field 의 default height 에 맞춰 button height 고정. + px 조금 줄임.
+ */
+export const button = `${buttonOutline} font-medium text-sm px-3 inline-block h-[42px]`
 
 export const buttonPrimary = `${button} ${primary}`
 
