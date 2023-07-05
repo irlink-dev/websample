f3b3fc7d7ac7cd337f2cfa9f10a529c4dad507de
leafdesk <lsg001008@gmail.com>
2023-07-05 18:30:36 +0900

    :construction: 작업 중 - Call, Recording, Message, Util 패널 탭 작업 중.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import CallPanel from '@/components/panels/CallPanel'
 import RecordingPanel from '@/components/panels/RecordingPanel'
 import MessagePanel from '@/components/panels/MessagePanel'
 import UtilPanel from '@/components/panels/UtilPanel'
-import { Grid, Typography } from '@mui/material'
 import { OcxStateContext } from '@/components/context/OcxStateContext'
 
 declare global {
@@ -28,23 +27,55 @@ const RootPage = () => {
         setOcx(() => ocx)
     }, [])
 
+    const TAB = 'inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50'
+    const TAB_SELECTED = 'inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg'
+
     return (
         <>
-            {/* 연결 패널 */}
             <ConnectionPanel ocx={ocx} />
 
-            {/* 콜 패널 */}
-            {/*<CallPanel ocx={ocx} />*/}
+            <div className="max-w-screen-lg mx-auto mt-5">
 
-            {/* 녹취 패널 */}
-            {/*<RecordingPanel ocx={ocx} />*/}
+                <div className="mb-4 border-b border-gray-200 dark:border-gray-700">
+                    <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200">
+                        <li className="mr-2">
+                            <button className={TAB_SELECTED}>
+                                Call Panel
+                            </button>
+                        </li>
+                        <li className="mr-2">
+                            <button className={TAB}>
+                                Recording Panel
+                            </button>
+                        </li>
+                        <li className="mr-2">
+                            <button className={TAB}>
+                                Message Panel
+                            </button>
+                        </li>
+                        <li>
+                            <button className={TAB}>
+                                Util Panel
+                            </button>
+                        </li>
+                    </ul>
+                </div>
 
-            {/* 문자 패널 */}
-            {/*<MessagePanel ocx={ocx} />*/}
-
-            {/* 유틸 패널 */}
-            {/*<UtilPanel ocx={ocx} />*/}
-
+                <div>
+                    <div className="hidden">
+                        <CallPanel ocx={ocx} />
+                    </div>
+                    <div className="hidden">
+                        <RecordingPanel ocx={ocx} />
+                    </div>
+                    <div className="hidden">
+                        <MessagePanel ocx={ocx} />
+                    </div>
+                    <div className="hidden">
+                        <UtilPanel ocx={ocx} />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
