5ba72661f7da21c7e4812e2e87c36ac209b0cca4
leafdesk <lsg001008@gmail.com>
2023-07-20 09:46:57 +0900

    :hammer: 코드 수정 - OcxState에 DndState 추가. - toStateKey 함수로 변환 함수들 통합. - 코드 가독성 개선.

--- src/types/OcxState.ts
+++ src/types/OcxState.ts
@@ -19,30 +19,32 @@ export const MuteState = {
     MIC_OFF: '1'
 }
 
+/**
+ * 착신 거부 상태.
+ */
+export const DndState = {
+    DND_OFF: '0',
+    DND_ON: '1'
+}
+
+/**
+ * String to state key.
+ */
+export const toStateKey = (
+    stateObject: { [key: string]: string },
+    state: string
+) => {
+    for (const [key, val] of Object.entries(stateObject)) {
+        if (val === state) {
+            return key
+        }
+    }
+    return undefined
+}
+
+export const toCreateDeviceState = (state: string) => toStateKey(CreateDeviceState, state)
 export type CreateDeviceStateType = typeof CreateDeviceState[keyof typeof CreateDeviceState]
-
+export const toMuteState = (state: string) => toStateKey(MuteState, state)
 export type MuteStateType = typeof MuteState[keyof typeof MuteState]
-
-/**
- * IR-WIRELESS(String) to CreateDeviceState.
- */
-export const toCreateDeviceState = (state: string) => {
-    for (const [key, val] of Object.entries(CreateDeviceState)) {
-        if (val === state) {
-            return key
-        }
-    }
-    return undefined
-}
-
-/**
- * (Number) to MuteState.
- */
-export const toMuteState = (state: string) => {
-    for (const [key, val] of Object.entries(MuteState)) {
-        if (val === state) {
-            return key
-        }
-    }
-    return undefined
-}
+export const toDndState = (state: string) => toStateKey(DndState, state)
+export type DndStateType = typeof DndState[keyof typeof DndState]
