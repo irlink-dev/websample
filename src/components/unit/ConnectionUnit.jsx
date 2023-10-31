7d0a1bdfa2f0401c729cfa00e12a3f1c04d6199a
leafdesk <lsg001008@gmail.com>
2023-10-31 15:16:56 +0900

    remove: 테스트 버튼 삭제

--- src/components/unit/ConnectionUnit.jsx
+++ src/components/unit/ConnectionUnit.jsx
@@ -26,10 +26,6 @@ const ConnectionUnit = () => {
 
   return (
     <div className="flex flex-col gap-2 w-full">
-      <button onClick={() => console.log(createDeviceState)}>
-        테스트 버튼
-      </button>
-
       <input
         placeholder="서버 URL"
         value={data.serverUrl}
